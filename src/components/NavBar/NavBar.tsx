785a3534a06730a56e6c99c209ef75b5de711be1
Ryan Foster-Hill <rfoster-hill@hotmail.com>
2024-02-24 11:45:46 +0000

    login modal finished

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,11 +3,12 @@ import "./NavBar.css";
 import hidePasswordIcon from "./NavBarAssets/hidepassicon.webp";
 import showPasswordIcon from "./NavBarAssets/showpassicon.png";
 import closeLoginWindowIcon from "./NavBarAssets/closeloginwindowicon.png";
+import loginTabIcon from "./NavBarAssets/logintabicon.png";
 import Login from '../Login';
 
 //Go mobile first
 //Add "hide navbar button", as well as a "open navbar button"
-
+//Make it so when it goes over the X value, it sets it to exactly the right number, rather than going +/- 20 pixels
 
 
 const NavBar = () => {
@@ -15,6 +16,8 @@ const NavBar = () => {
     let [hidePass, setHidePass] = useState(true);
     let [loginModal, setLoginModal] = useState("");
     let loginModalEle = document.getElementById("loginModal");
+    let loginTab = document.getElementById("loginTab");
+    
 
     //Login modal animation
     let x: number;
@@ -29,18 +32,18 @@ const NavBar = () => {
         // console.log("no rounding:",(window.innerWidth / 2) - (loginModalEle.offsetWidth / 2))
 
     }
-    let id: NodeJS.Timer;
+    let loginModalTimer: NodeJS.Timer;
     if (loginModal === "closed") {
-        id = setInterval(closeLoginModal, 1);
+        loginModalTimer = setInterval(closeLoginModal, 1);
     } else if (loginModal === "open") {
-        id = setInterval(openLoginModal, 1);
+        loginModalTimer = setInterval(openLoginModal, 1);
     }
 
     function closeLoginModal() {
         console.log("close modal frame running");
         if (loginModalEle) {
             if (x <= -300) {
-                clearInterval(id);
+                clearInterval(loginModalTimer);
             } else {
                 x -= 20;
                 loginModalEle.style.left = x + "px";
@@ -55,7 +58,10 @@ const NavBar = () => {
         if (loginModalEle) {
             //Place the modal in the middle of the screen and then remove the width of the modal so it's centered in the screen, the round
             if (x >= Math.round((window.innerWidth / 2) - (loginModalEle.offsetWidth / 2))) {
-                clearInterval(id);
+                clearInterval(loginModalTimer);
+                if (loginModalEle.parentElement) {
+                    loginModalEle.style.removeProperty("left"); //Remove left property so modal is centered again
+                }
                 console.log("ending X = :", x);
             } else {
                 x += 20;
@@ -66,9 +72,36 @@ const NavBar = () => {
         }
     }
 
+    // let loginTabTimer: NodeJS.Timer;
+    // if (loginModal === "closed") {
+    //     loginModalTimer = setInterval(closeLoginModal, 1);
+    // } else if (loginModal === "open") {
+    //     loginModalTimer = setInterval(openLoginModal, 1);
+    // }
+
+    // function moveLoginTab() {
+    //     console.log("move login tab frame running");
+    //     if (loginModalEle) {
+    //         if (x <= -300) {
+    //             clearInterval(loginModalTimer);
+    //         } else {
+    //             x -= 20;
+    //             loginModalEle.style.left = x + "px";
+    //         }
+    //     } else {
+    //         console.log("modal not found in moveLoginTab");
+    //     }
+    // }
+
     return (
         <div className='flex items-center justify-center h-screen w-screen absolute'>
-            <button onClick={(event) => { setLoginModal("open"); event.preventDefault(); }} >BRING IT BACK</button>
+            <button onClick={(event) => { setLoginModal("open"); event.preventDefault(); }} id='loginTab' className={`fixed flex items-center justify-evenly top-[100px] bg-green-400 rounded-r-3xl w-[150px] h-[60px] text-2xl`}>
+                Login
+                <img src={loginTabIcon} alt='loginTabIcon' className='w-[25px] bg-white rounded-lg' />
+            </button>
+
+
+
             <div id='loginModal' className={`fixed bg-green-300 rounded-xl border-green-500 border-2 min-h-[300px]`}>
 
                 <div className='flex justify-evenly w-full min-h-[50px] border-b-2 border-green-500'>
@@ -103,9 +136,9 @@ const NavBar = () => {
                                         <div className='absolute top-0 right-0 mr-[5px]' onClick={() => { setHidePass(!hidePass) }}>
                                             {hidePass
                                                 ?
-                                                (<img src={hidePasswordIcon} alt='hidepasswordicon' className='w-[25px]' />)
-                                                :
                                                 (<img src={showPasswordIcon} alt='hidepasswordicon' className='w-[25px]' />)
+                                                :
+                                                (<img src={hidePasswordIcon} alt='hidepasswordicon' className='w-[25px]' />)
                                             }
                                         </div>
                                     </div>
@@ -116,6 +149,36 @@ const NavBar = () => {
 
                     {selectedTab === "Register" &&
                         <div>
+                            <div className='h-full'>
+                                <form className='flex flex-col items-center h-2/4'>
+                                    <div className='w-full flex items-center justify-center'>
+                                        <button onClick={(event) => { setLoginModal("closed"); event.preventDefault(); }} className='absolute left-0 ml-[10px]'>
+                                            <img src={closeLoginWindowIcon} className='w-[20px]' alt='closeloginwindowicon' />
+                                        </button>
+                                        <h1 className='text-3xl'>Register</h1>
+                                    </div>
+
+                                    <div className='flex flex-col items-center mt-[20px]'>
+                                        <label>Username</label>
+                                        <input type='text'></input>
+                                    </div>
+
+                                    <div className='flex flex-col items-center'>
+                                        <label>Password</label>
+                                        <div className='relative'>
+                                            <input type={hidePass ? "password" : "text"} className=''></input>
+                                            <div className='absolute top-0 right-0 mr-[5px]' onClick={() => { setHidePass(!hidePass) }}>
+                                                {hidePass
+                                                    ?
+                                                    (<img src={showPasswordIcon} alt='hidepasswordicon' className='w-[25px]' />)
+                                                    :
+                                                    (<img src={hidePasswordIcon} alt='hidepasswordicon' className='w-[25px]' />)
+                                                }
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     }
 
